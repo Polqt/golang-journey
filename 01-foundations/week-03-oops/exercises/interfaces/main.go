bea12d2ce05b76eb4ac754e10b576472aea68303
Jepoy <poyhidalgo@gmail.com>
2026-01-28 18:11:36 +0800

    chore: done learning interfaces and structs. maybe add more example in structs

--- 01-foundations/week-03-oops/exercises/interfaces/main.go
+++ 01-foundations/week-03-oops/exercises/interfaces/main.go
@@ -1,1 +1,68 @@
 package main
+
+import "fmt"
+
+// Define Interface
+type Stringer interface {
+	String() string
+}
+
+// Implement interface
+type Person struct {
+	Name string
+}
+
+func (p Person) String() string {
+	return p.Name
+}
+
+// Using Interface
+func PrintString(s Stringer) {
+	fmt.Println(s.String())
+}
+
+// Empty Interface
+func PrintAnything(v interface{}) {
+	fmt.Println(v)
+}
+
+// Interface Compliance
+type Shape interface {
+	Area() float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return 3.14 * c.Radius * c.Radius
+}
+
+func main() {
+	// Type Assertions
+	// Type assertions are used to retrieve the dynamic type of an interface. 
+	// They are useful when you need to work with the underlying type of an interface value.
+	var i interface{} = "hello"
+
+	p := Person{Name: "Ben"}
+	PrintString(p)
+	fmt.Println("")
+
+	PrintAnything("Hi")
+	PrintAnything(12345)
+	PrintAnything([]int{1, 2, 3})
+
+	fmt.Println("")
+
+	s, ok := i.(string) 
+	if ok {
+		fmt.Println("String value:",s)
+	} else {
+		fmt.Println("Not a string")
+	}
+
+	fmt.Println("")
+	c := Circle{Radius: 5}
+	fmt.Printf("Circle area: %.2f\n", c.Area())
+}
--- 01-foundations/week-03-oops/exercises/struc-composition/main.go
+++ 01-foundations/week-03-oops/exercises/struc-composition/main.go
@@ -1,1 +1,32 @@
 package main
+
+import "fmt"
+
+type Person struct {
+	name string
+	age  int
+}
+
+func main() {
+	p := Person{
+		name: "John",
+		age:  25,
+	}
+
+	p1 := Person{
+		name: "Alice",
+		age:  30,
+	}
+	fmt.Printf("%s: %d", p1.name, p1.age)
+
+	fmt.Println()
+
+	modifyPerson(&p)
+	println("Modified name:", p.name)
+
+	
+}
+
+func modifyPerson(p *Person) {
+	p.name = "Bob"
+}
